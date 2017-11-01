7546a2a198c74abf4c3b9a5cfebe2ef40439655d
Morgan Fudge <mfudge92@gmail.com>
2017-11-01 12:40:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    if(array.includes?(cheese))
+    if(array.include?(cheese))
       return true
     end
   end
